17cebeb3af226f81279409a36b255d4a619097d3
Yoon Wai Yan <waiyan@cloudcoder.com.my>
2015-04-02 18:18:22 +0800

    add datatable.rb

--- lib/rails_baseline/template.rb
+++ lib/rails_baseline/template.rb
@@ -487,6 +487,64 @@ datatable_config_lines_non_bootstrap = <<-TEXT
 @import "dataTables/jquery.dataTables";
 TEXT
 
+datatable_model = <<-TEXT
+class Datatable
+  include ApplicationHelper
+  delegate :params, :t, :h, :current_user, :link_to, to: :@view
+
+  def initialize(view)
+    @view = view
+  end
+
+  def as_json(options = {})
+    {
+      sEcho: params[:sEcho].to_i,
+      iTotalRecords: rows ? rows.count : 0,
+      iTotalDisplayRecords: rows ? rows.total_entries : 0,
+      aaData: data
+    }
+  end
+
+
+  def rows
+    @rows ||= fetch_datas
+  end
+
+  def fetch_datas
+  end
+
+  def page
+    params[:iDisplayStart].to_i/per_page + 1
+  end
+
+  def per_page
+    params[:iDisplayLength].to_i > 0 ? params[:iDisplayLength].to_i : 10
+  end
+
+  def columns
+    %w[]
+  end
+
+  def sort_column
+    if params["iSortCol_0"].blank?
+      columns[params[:order]["0"]["column"].to_i]
+    else 
+      columns[params["iSortCol_0"].to_i]    
+    end
+    
+  end
+
+  def sort_direction
+    if params["iSortCol_0"].blank?
+      params[:order]["0"]["dir"] == "desc" ? "desc" : "asc"
+    else
+      params["sSortDir_0"] == "desc" ? "desc" : "asc"
+    end
+    
+  end
+end
+TEXT
+
 after_bundler do
   after_bundler do
     if @configs["bootstrap"] # if bootstrap configuration is true
@@ -501,6 +559,7 @@ after_bundler do
         "\n//= require dataTables/jquery.dataTables\n//= require dataTables/bootstrap/3/jquery.dataTables.bootstrap"
       end
     end
+    create_file "app/models/datatable.rb", datatable_model
   end
 end
 
