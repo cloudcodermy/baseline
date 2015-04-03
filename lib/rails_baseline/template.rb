b495ec090dbf2b6291db75cccc82c534cf3b0899
Yoon Wai Yan <waiyan@cloudcoder.com.my>
2015-04-03 15:16:50 +0800

    add Font Awesome, Liquid template engine and deployment methods

--- lib/rails_baseline/template.rb
+++ lib/rails_baseline/template.rb
@@ -457,6 +457,31 @@ if use_bootstrap
   end
 end
 
+# >-----------------------------[ Font Awesome ]------------------------------<
+
+@current_recipe = "font-awesome"
+@before_configs["font-awesome"].call if @before_configs["font-awesome"]
+say_recipe 'Font Awesome'
+
+font_awesome_configs = <<-TEXT
+@import "font-awesome-sprockets";
+@import "font-awesome";
+TEXT
+
+gem 'font-awesome-sass', '~> 4.3.0'
+
+after_bundler do
+  append_to_file 'app/assets/stylesheets/application.css.scss', font_awesome_configs
+end
+
+# >-----------------------------[ Liquid ]------------------------------<
+
+@current_recipe = "liquid"
+@before_configs["liquid"].call if @before_configs["liquid"]
+say_recipe 'Liquid'
+
+gem "liquid"
+
 # >--------------------------[ jQuery Validation ]-----------------------------<
 
 @current_recipe = "jquery-validation-rails"
@@ -584,6 +609,71 @@ say_recipe 'Home Controller'
 after_bundler do
   generate "controller home index"
   inject_into_file "config/routes.rb", "\n  root 'home#index'", :after => "devise_for :users"
+end
+
+# >---------------------------[ Deployment ]----------------------------<
+
+@current_recipe = "deployment"
+@before_configs["deployment"].call if @before_configs["deployment"]
+say_recipe 'Deployment'
+
+config['deployment'] = multiple_choice("Which deployment method are you using?", [["Heroku", "heroku"], ["Capistrano", "capistrano"], ["Engine Yard", "engineyard"]])
+
+unicorn_config =<<-TEXT
+# config/unicorn.rb
+worker_processes Integer(ENV["WEB_CONCURRENCY"] || 3)
+timeout 15
+preload_app true
+
+before_fork do |server, worker|
+  Signal.trap 'TERM' do
+    puts 'Unicorn master intercepting TERM and sending myself QUIT instead'
+    Process.kill 'QUIT', Process.pid
+  end
+
+  defined?(ActiveRecord::Base) and
+    ActiveRecord::Base.connection.disconnect!
+end
+
+after_fork do |server, worker|
+  Signal.trap 'TERM' do
+    puts 'Unicorn worker intercepting TERM and doing nothing. Wait for master to send QUIT'
+  end
+
+  defined?(ActiveRecord::Base) and
+    ActiveRecord::Base.establish_connection
+end
+TEXT
+
+ey_config = <<-TEXT
+---
+# This is all you need for a typical rails application.
+defaults:
+  migrate: true
+  migration_command: rake db:migrate
+  precompile_assets: true
+TEXT
+
+case config['deployment']
+when "heroku"
+  gem 'unicorn'
+  gem 'rails_12factor', group: :production
+
+  after_bundler do
+    create_file "config/unicorn.rb", unicorn_config
+    create_file "Procfile", "web: bundle exec unicorn -p $PORT -c ./config/unicorn.rb"
+  end
+when "capistrano"
+  gem 'capistrano', '~> 3.3.0'
+
+  after_bundler do
+    run "bundle exec cap install"
+  end
+
+when "engineyard"
+  after_bundler do
+    create_file "config/ey.yml", ey_config
+  end
 end
 
 # >----------------------------------[ Git ]----------------------------------<
